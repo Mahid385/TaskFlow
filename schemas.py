2a6b672605f47e169ae4c660b94583b06b8e8f68
Mahid385 <mahidsajjad385@gmail.com>
2026-08-21 19:42:24 +0500

    feat(tasks): add Pydantic request and response schemas

---
+++ schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel,ConfigDict
+
+
+class TaskCreate(BaseModel):
+    title: str
+    description: str
+
+
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+
+
+class TaskResponse(BaseModel):
+    id: str
+    title: str
+    description: str
+    user_id: str
+    model_config=ConfigDict(from_attributes=True)
+
+class TaskListResponse(BaseModel):
+    user_id: str
+    tasks: list[TaskResponse]
