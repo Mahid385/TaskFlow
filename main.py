4e49ed0f9b29bf1538627e285a9a24f4b3bba2f3
Mahid385 <mahidsajjad385@gmail.com>
2026-09-02 08:47:58 +0500

    configure CORS for frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from auth import router as auth_router
 from task import router as task_router
@@ -8,8 +9,18 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 app.include_router(task_router)
 
 if __name__=="__main__":
-    uvicorn.run("main:app",reload=True,host="127.0.0.1",port=800)
+    uvicorn.run("main:app",reload=True,host="127.0.0.1",port=8000)
